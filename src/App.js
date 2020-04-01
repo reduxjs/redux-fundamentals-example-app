52dce35454d40f3e6291193c0b043eb224f29b16
Mark Erikson <mark@isquaredsoftware.com>
2020-03-31 20:26:47 -0400

    Initial example project setup

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <section>
+          <h1>Redux Fundamentals Example</h1>
+
+          <div className="navContent">
+            <div className="navLinks"></div>
+          </div>
+        </section>
+      </nav>
+      <section>
+        <h2>Welcome to the Redux Fundamentals example app!</h2>
+      </section>
+    </div>
+  )
+}
+
+export default App
