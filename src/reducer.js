d7dcc00e3eb976fdeb96bdb740ba6c7e10dd244f
Mark Erikson <mark@isquaredsoftware.com>
2020-10-03 13:16:19 -0400

    Add reducer case for statusFilterChanged action

--- src/reducer.js
+++ src/reducer.js
@@ -32,22 +32,30 @@ export default function appReducer(state = initialState, action) {
     }
     case 'todos/todoToggled': {
       return {
-        // Again copy the entire state object
         ...state,
-        // This time, we need to make a copy of the old todos array
         todos: state.todos.map((todo) => {
-          // If this isn't the todo item we're looking for, leave it alone
           if (todo.id !== action.payload) {
             return todo
           }
 
-          // We've found the todo that has to change. Return a copy:
           return {
             ...todo,
-            // Flip the completed flag
             completed: !todo.completed,
           }
         }),
+      }
+    }
+    case 'filters/statusFilterChanged': {
+      return {
+        // Copy the whole state
+        ...state,
+        // Overwrite the filters value
+        filters: {
+          // copy the other filter fields
+          ...state.filters,
+          // And replace the status field with the new value
+          status: action.payload,
+        },
       }
     }
     default:
