9ddc12681adc1bcedf8b1fa576ad8e524f9f4867
Mark Erikson <mark@isquaredsoftware.com>
2020-10-03 14:14:30 -0400

    Add and use sayHi store enhancer

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { createStore } from 'redux'
 import rootReducer from './reducer'
+import { sayHiOnDispatch } from './exampleAddons/enhancers'
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, undefined, sayHiOnDispatch)
 
 export default store
