7d17ec4fa5d46c839adfd8c716ccfedaf365f7c0
Mark Erikson <mark@isquaredsoftware.com>
2020-10-03 14:14:30 -0400

    Add and use sayHi store enhancer

--- src/index.js
+++ src/index.js
@@ -7,38 +7,9 @@ import './api/server'
 
 import store from './store'
 
-// Log the initial state
-console.log('Initial state: ', store.getState())
-// {todos: [....], filters: {status, colors}}
-
-// Every time the state changes, log it
-// Note that subscribe() returns a function for unregistering the listener
-const unsubscribe = store.subscribe(() =>
-  console.log('State after dispatch: ', store.getState())
-)
-
-// Now, dispatch some actions
-
+console.log('Dispatching action')
 store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about actions' })
-store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about reducers' })
-store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about stores' })
-
-store.dispatch({ type: 'todos/todoToggled', payload: 0 })
-store.dispatch({ type: 'todos/todoToggled', payload: 1 })
-
-store.dispatch({ type: 'filters/statusFilterChanged', payload: 'Completed' })
-
-store.dispatch({
-  type: 'filters/colorFilterChanged',
-  payload: { color: 'red', changeType: 'selected' },
-})
-
-// Stop listening to state updates
-unsubscribe()
-
-// Dispatch one more action to see what happens
-
-store.dispatch({ type: 'todos/todoAdded', payload: 'Try creating a store' })
+console.log('Dispatch complete')
 
 ReactDOM.render(
   <React.StrictMode>
