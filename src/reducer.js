bf6d30554ecbf92402942016fc7349b273edee71
Mark Erikson <mark@isquaredsoftware.com>
2020-10-03 13:08:18 -0400

    Add reducer case for todoAdded action

--- src/reducer.js
+++ src/reducer.js
@@ -10,11 +10,35 @@ const initialState = {
   },
 }
 
+function nextTodoId(todos) {
+  const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1)
+  return maxId + 1
+}
+
 // Use the initialState as a default value
 export default function appReducer(state = initialState, action) {
   // The reducer normally looks at the action type field to decide what happens
   switch (action.type) {
     // Do something here based on the different types of actions
+    case 'todos/todoAdded': {
+      // We need to return a new state object
+      return {
+        // that has all the existing state data
+        ...state,
+        // but has a new array for the `todos` field
+        todos: [
+          // with all of the old todos
+          ...state.todos,
+          // and the new todo object
+          {
+            // Use an auto-incrementing numeric ID for this example
+            id: nextTodoId(state.todos),
+            text: action.payload,
+            completed: false,
+          },
+        ],
+      }
+    }
     default:
       // If this reducer doesn't recognize the action type, or doesn't
       // care about this specific action, return the existing state unchanged
