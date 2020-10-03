86b171261e61416980074e251c032613fef937c1
Mark Erikson <mark@isquaredsoftware.com>
2020-10-03 13:25:18 -0400

    Update the root reducer to use combineReducers from Redux

--- src/reducer.js
+++ src/reducer.js
@@ -1,12 +1,12 @@
+import { combineReducers } from 'redux'
+
 import todosReducer from './features/todos/todosSlice'
 import filtersReducer from './features/filters/filtersSlice'
 
-export default function rootReducer(state, action) {
-  // always return a new object for the root state
-  return {
-    // the value of `state.todos` is whatever the todos reducer returns
-    todos: todosReducer(state.todos, action),
-    // For both reducers, we only pass in their slice of the state
-    filters: filtersReducer(state.filters, action),
-  }
-}
+const rootReducer = combineReducers({
+  // Define a top-level state field named `todos`, handled by `todosReducer`
+  todos: todosReducer,
+  filters: filtersReducer,
+})
+
+export default rootReducer
