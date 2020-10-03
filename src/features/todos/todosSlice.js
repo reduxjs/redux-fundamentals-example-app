e0db1c096bfbc05cff4f8efb03797bfa616bdbd0
Mark Erikson <mark@isquaredsoftware.com>
2020-10-03 13:19:03 -0400

    Move todos reducer logic to todosSlice

--- src/features/todos/todosSlice.js
+++ src/features/todos/todosSlice.js
@@ -11,6 +11,29 @@ function nextTodoId(todos) {
 
 export default function todosReducer(state = initialState, action) {
   switch (action.type) {
+    case 'todos/todoAdded': {
+      // Can return just the new todos array - no extra object around it
+      return [
+        ...state,
+        {
+          id: nextTodoId(state),
+          text: action.payload,
+          completed: false,
+        },
+      ]
+    }
+    case 'todos/todoToggled': {
+      return state.map((todo) => {
+        if (todo.id !== action.payload) {
+          return todo
+        }
+
+        return {
+          ...todo,
+          completed: !todo.completed,
+        }
+      })
+    }
     default:
       return state
   }
