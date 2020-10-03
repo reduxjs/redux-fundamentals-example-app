a8dafc2526b37c28f63eedb47875ab7cf3cc9559
Mark Erikson <mark@isquaredsoftware.com>
2020-10-03 13:17:30 -0400

    Create todosSlice file

---
+++ src/features/todos/todosSlice.js
@@ -0,0 +1,17 @@
+const initialState = [
+  { id: 0, text: 'Learn React', completed: true },
+  { id: 1, text: 'Learn Redux', completed: false, color: 'purple' },
+  { id: 2, text: 'Build something fun!', completed: false, color: 'blue' },
+]
+
+function nextTodoId(todos) {
+  const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1)
+  return maxId + 1
+}
+
+export default function todosReducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
