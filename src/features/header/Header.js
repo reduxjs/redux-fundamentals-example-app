c662f09a975198d9ec55c7bb07243da3e46bbcae
Mark Erikson <mark@isquaredsoftware.com>
2020-10-04 14:01:36 -0400

    Use React-Redux hooks and Provider in UI

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -1,9 +1,22 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
 
 const Header = () => {
   const [text, setText] = useState('')
+  const dispatch = useDispatch()
 
   const handleChange = (e) => setText(e.target.value)
+
+  const handleKeyDown = (e) => {
+    // If the user pressed the Enter key:
+    const trimmedText = text.trim()
+    if (e.which === 13 && trimmedText) {
+      // Dispatch the "todo added" action with this text
+      dispatch({ type: 'todos/todoAdded', payload: trimmedText })
+      // And clear out the text input
+      setText('')
+    }
+  }
 
   return (
     <header className="header">
@@ -12,6 +25,7 @@ const Header = () => {
         placeholder="What needs to be done?"
         value={text}
         onChange={handleChange}
+        onKeyDown={handleKeyDown}
       />
     </header>
   )
