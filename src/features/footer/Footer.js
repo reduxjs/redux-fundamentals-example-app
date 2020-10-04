9956cc2e8b5a739f3c695aa853b112886b0047cc
Mark Erikson <mark@isquaredsoftware.com>
2020-10-04 15:02:44 -0400

    Update Footer to dispatch completion and status actions

--- src/features/footer/Footer.js
+++ src/features/footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 import { availableColors, capitalize } from '../filters/colors'
 import { StatusFilters } from '../filters/filtersSlice'
@@ -74,6 +74,8 @@ const ColorFilters = ({ value: colors, onChange }) => {
 }
 
 const Footer = () => {
+  const dispatch = useDispatch()
+
   const todosRemaining = useSelector((state) => {
     const uncompletedTodos = state.todos.filter((todo) => !todo.completed)
     return uncompletedTodos.length
@@ -81,16 +83,29 @@ const Footer = () => {
 
   const { status, colors } = useSelector((state) => state.filters)
 
+  const onMarkCompletedClicked = () => dispatch({ type: 'todos/allCompleted' })
+  const onClearCompletedClicked = () =>
+    dispatch({ type: 'todos/completedCleared' })
+
   const onColorChange = (color, changeType) =>
-    console.log('Color change: ', { color, changeType })
-  const onStatusChange = (status) => console.log('Status change: ', status)
+    dispatch({
+      type: 'filters/colorFilterChanged',
+      payload: { color, changeType },
+    })
+
+  const onStatusChange = (status) =>
+    dispatch({ type: 'filters/statusFilterChanged', payload: status })
 
   return (
     <footer className="footer">
       <div className="actions">
         <h5>Actions</h5>
-        <button className="button">Mark All Completed</button>
-        <button className="button">Clear Completed</button>
+        <button className="button" onClick={onMarkCompletedClicked}>
+          Mark All Completed
+        </button>
+        <button className="button" onClick={onClearCompletedClicked}>
+          Clear Completed
+        </button>
       </div>
 
       <RemainingTodos count={todosRemaining} />
