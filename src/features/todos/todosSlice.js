23b843e7df84e710d21e45ad0a0a41fe8c4cf206
Mark Erikson <mark@isquaredsoftware.com>
2020-10-18 14:20:26 -0400

    Add saveNewTodo thunk

--- src/features/todos/todosSlice.js
+++ src/features/todos/todosSlice.js
@@ -69,3 +69,11 @@ export async function fetchTodos(dispatch, getState) {
   const response = await client.get('/fakeApi/todos')
   dispatch({ type: 'todos/todosLoaded', payload: response.todos })
 }
+
+export function saveNewTodo(text) {
+  return async function saveNewTodoThunk(dispatch, getState) {
+    const initialTodo = { text }
+    const response = await client.post('/fakeApi/todos', { todo: initialTodo })
+    dispatch({ type: 'todos/todoAdded', payload: response.todo })
+  }
+}
