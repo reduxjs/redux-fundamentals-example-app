255fefb03912003fa7b5cf2030a7047b6737855e
Mark Erikson <mark@isquaredsoftware.com>
2020-10-18 15:43:58 -0400

    Update todos slice to handle changed todoAdded action

--- src/features/todos/todosSlice.js
+++ src/features/todos/todosSlice.js
@@ -2,23 +2,10 @@ import { client } from '../../api/client'
 
 const initialState = []
 
-function nextTodoId(todos) {
-  const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1)
-  return maxId + 1
-}
-
 export default function todosReducer(state = initialState, action) {
   switch (action.type) {
     case 'todos/todoAdded': {
-      // Can return just the new todos array - no extra object around it
-      return [
-        ...state,
-        {
-          id: nextTodoId(state),
-          text: action.payload,
-          completed: false,
-        },
-      ]
+      return [...state, action.payload]
     }
     case 'todos/todoToggled': {
       return state.map((todo) => {
