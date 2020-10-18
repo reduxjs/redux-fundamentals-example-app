3c41899f6090a2b446faf4921d78e82288dbd8ef
Mark Erikson <mark@isquaredsoftware.com>
2020-10-18 13:18:08 -0400

    Configure the store to use the thunk middleware

--- src/store.js
+++ src/store.js
@@ -1,11 +1,10 @@
 import { createStore, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './reducer'
-import { print1, print2, print3 } from './exampleAddons/middleware'
 
 const composedEnhancer = composeWithDevTools(
-  // Add whatever middleware you actually want to use here
-  applyMiddleware()
+  applyMiddleware(thunkMiddleware)
   // other store enhancers if any
 )
 
