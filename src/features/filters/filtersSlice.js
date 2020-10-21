db6bbaa5c4ab6e16f0fd0f37024859a34f9a3d28
Mark Erikson <mark@isquaredsoftware.com>
2020-10-20 21:35:32 -0400

    Add action creator for colorFilterChanged

--- src/features/filters/filtersSlice.js
+++ src/features/filters/filtersSlice.js
@@ -50,3 +50,10 @@ export default function filtersReducer(state = initialState, action) {
       return state
   }
 }
+
+export const colorFilterChanged = (color, changeType) => {
+  return {
+    type: 'filters/colorFilterChanged',
+    payload: { color, changeType },
+  }
+}
--- src/features/footer/Footer.js
+++ src/features/footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
 import { availableColors, capitalize } from '../filters/colors'
-import { StatusFilters } from '../filters/filtersSlice'
+import { StatusFilters, colorFilterChanged } from '../filters/filtersSlice'
 
 const RemainingTodos = ({ count }) => {
   const suffix = count === 1 ? '' : 's'
@@ -88,10 +88,7 @@ const Footer = () => {
     dispatch({ type: 'todos/completedCleared' })
 
   const onColorChange = (color, changeType) =>
-    dispatch({
-      type: 'filters/colorFilterChanged',
-      payload: { color, changeType },
-    })
+    dispatch(colorFilterChanged(color, changeType))
 
   const onStatusChange = (status) =>
     dispatch({ type: 'filters/statusFilterChanged', payload: status })
