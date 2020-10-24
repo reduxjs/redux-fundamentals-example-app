04e34c9c9230ac00e25521f36f99bc17ec38f0e6
Mark Erikson <mark@isquaredsoftware.com>
2020-10-24 14:39:34 -0400

    Add request state field to todos slice

--- src/features/todos/todosSlice.js
+++ src/features/todos/todosSlice.js
@@ -2,51 +2,75 @@ import { createSelector } from 'reselect'
 import { client } from '../../api/client'
 import { StatusFilters } from '../filters/filtersSlice'
 
-const initialState = []
+const initialState = {
+  status: 'idle',
+  entities: [],
+}
 
 export default function todosReducer(state = initialState, action) {
   switch (action.type) {
     case 'todos/todoAdded': {
-      return [...state, action.payload]
+      return {
+        ...state,
+        entities: [...state.entities, action.payload],
+      }
     }
     case 'todos/todoToggled': {
-      return state.map((todo) => {
-        if (todo.id !== action.payload) {
-          return todo
-        }
+      return {
+        ...state,
+        entities: state.entities.map((todo) => {
+          if (todo.id !== action.payload) {
+            return todo
+          }
 
-        return {
-          ...todo,
-          completed: !todo.completed,
-        }
-      })
+          return {
+            ...todo,
+            completed: !todo.completed,
+          }
+        }),
+      }
     }
     case 'todos/colorSelected': {
       const { color, todoId } = action.payload
-      return state.map((todo) => {
-        if (todo.id !== todoId) {
-          return todo
-        }
+      return {
+        ...state,
+        entities: state.entities.map((todo) => {
+          if (todo.id !== todoId) {
+            return todo
+          }
 
-        return {
-          ...todo,
-          color,
-        }
-      })
+          return {
+            ...todo,
+            color,
+          }
+        }),
+      }
     }
     case 'todos/todoDeleted': {
-      return state.filter((todo) => todo.id !== action.payload)
+      return {
+        ...state,
+        entities: state.entities.filter((todo) => todo.id !== action.payload),
+      }
     }
     case 'todos/allCompleted': {
-      return state.map((todo) => {
-        return { ...todo, completed: true }
-      })
+      return {
+        ...state,
+        entities: state.entities.map((todo) => {
+          return { ...todo, completed: true }
+        }),
+      }
     }
     case 'todos/completedCleared': {
-      return state.filter((todo) => !todo.completed)
+      return {
+        ...state,
+        entities: state.entities.filter((todo) => !todo.completed),
+      }
     }
     case 'todos/todosLoaded': {
-      return action.payload
+      return {
+        ...state,
+        entities: action.payload,
+      }
     }
     default:
       return state
@@ -93,7 +117,7 @@ export function saveNewTodo(text) {
   }
 }
 
-export const selectTodos = (state) => state.todos
+export const selectTodos = (state) => state.todos.entities
 
 export const selectTodoById = (state, todoId) => {
   return selectTodos(state).find((todo) => todo.id === todoId)
