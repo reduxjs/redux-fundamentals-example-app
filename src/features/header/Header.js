802848b93646873d03a5d4fea7f954b7f0f4a5b6
Mark Erikson <mark@isquaredsoftware.com>
2020-10-24 15:29:15 -0400

    Show loading spinner in header based on thunk promise

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -5,30 +5,39 @@ import { saveNewTodo } from '../todos/todosSlice'
 
 const Header = () => {
   const [text, setText] = useState('')
+  const [status, setStatus] = useState('idle')
   const dispatch = useDispatch()
 
   const handleChange = (e) => setText(e.target.value)
 
-  const handleKeyDown = (e) => {
+  const handleKeyDown = async (e) => {
     // If the user pressed the Enter key:
     const trimmedText = text.trim()
     if (e.which === 13 && trimmedText) {
       // Create and dispatch the thunk function itself
-      dispatch(saveNewTodo(trimmedText))
+      setStatus('loading')
+      await dispatch(saveNewTodo(trimmedText))
       // And clear out the text input
       setText('')
+      setStatus('idle')
     }
   }
+
+  let isLoading = status === 'loading'
+  let placeholder = isLoading ? '' : 'What needs to be done?'
+  let loader = isLoading ? <div className="loader" /> : null
 
   return (
     <header className="header">
       <input
         className="new-todo"
-        placeholder="What needs to be done?"
+        placeholder={placeholder}
         value={text}
         onChange={handleChange}
         onKeyDown={handleKeyDown}
+        disabled={isLoading}
       />
+      {loader}
     </header>
   )
 }
