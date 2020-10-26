981efefca5307686352694258eff9e52743f6ed2
Mark Erikson <mark@isquaredsoftware.com>
2020-10-25 21:41:37 -0400

    Replace existing store setup with RTK's configureStore

--- src/store.js
+++ src/store.js
@@ -1,12 +1,14 @@
-import { createStore, applyMiddleware } from 'redux'
-import thunkMiddleware from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import rootReducer from './reducer'
+import { configureStore } from '@reduxjs/toolkit'
 
-const composedEnhancer = composeWithDevTools(
-  applyMiddleware(thunkMiddleware)
-  // other store enhancers if any
-)
+import todosReducer from './features/todos/todosSlice'
+import filtersReducer from './features/filters/filtersSlice'
 
-const store = createStore(rootReducer, composedEnhancer)
+const store = configureStore({
+  reducer: {
+    // Define a top-level state field named `todos`, handled by `todosReducer`
+    todos: todosReducer,
+    filters: filtersReducer,
+  },
+})
+
 export default store
